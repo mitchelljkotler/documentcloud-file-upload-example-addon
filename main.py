8f9e9e28f59182b5b92e5e6eaf937f587128e224
Mitchell Kotler <mitch@mitchellkotler.net>
2024-08-02 15:46:32 -0400

    init

--- main.py
+++ main.py
@@ -1,38 +1,26 @@
 """
-This is a hello world add-on for DocumentCloud.
+This is a file upload add-on for DocumentCloud.
 
-It demonstrates how to write a add-on which can be activated from the
-DocumentCloud add-on system and run using Github Actions.  It receives data
-from DocumentCloud via the request dispatch and writes data back to
-DocumentCloud using the standard API
+It demonstrates how to upload a file directly to an add-on.
+It then uploads that file to DocumentCloud.
 """
 
 from documentcloud.addon import AddOn
 
 
-class HelloWorld(AddOn):
-    """An example Add-On for DocumentCloud."""
+class FileUpload(AddOn):
+    """A file upload Add-On for DocumentCloud."""
 
     def main(self):
-        """The main add-on functionality goes here."""
-        # fetch your add-on specific data
-        name = self.data.get("name", "world")
 
-        self.set_message("Hello World start!")
+        file_id = self.data["file_id"]
+        self.set_message(f"File ID {file_id}")
+        file_name = self.download_file(file_id)
+        self.set_message(f"File name {file_name}")
 
-        # add a hello note to the first page of each selected document
-        for document in self.get_documents():
-            # get_documents will iterate through all documents efficiently,
-            # either selected or by query, dependeing on which is passed in
-            document.annotations.create(f"Hello {name}!", 0)
-
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
-            self.upload_file(file_)
-
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
+        self.client.documents.upload(file_name)
+        self.set_message(f"{file_name} uploaded")
 
 
 if __name__ == "__main__":
-    HelloWorld().main()
+    FileUpload().main()
